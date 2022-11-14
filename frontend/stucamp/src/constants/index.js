45484d366b33960ce7c5c47295bb3a6c5c1ae5c7
Adebayo Samuel <eladebayoor@gmail.com>
2022-11-14 18:56:35 +0100

    [feat]: constant data file created

---
+++ frontend/stucamp/src/constants/index.js
@@ -0,0 +1,85 @@
+
+
+
+export const navLinks = [
+    {
+      id: "home",
+      title: "Home",
+    },
+    {
+      id: "apartments",
+      title: "Apartments",
+    },
+    {
+      id: "login",
+      title: "Login",
+    },
+  ];
+
+export const reviews = [
+    {
+      id: "reviews-1",
+      content:
+        "It was a home away from home experience",
+      name: "Sultan",
+      title: "Postgraduate Student of Ahmadu Bello University",
+      img: people01,
+    },
+    {
+      id: "reviews-2",
+      content:
+        "I had a great 4 years during my time at the apartment",
+      name: "Abraham",
+      title: "University of Benin student",
+      img: people02,
+    },
+    {
+      id: "reviews-3",
+      content:
+        "The customer service and support was amazing, I recomment stuCamp.",
+      name: "Omolola",
+      title: "University of Lagos student",
+      img: people03,
+    },
+  ];
+
+export const stats = [
+    {
+      id: "stats-1",
+      title: "Customer service",
+      value: "+50k",
+    },
+    {
+      id: "stats-2",
+      title: "Positive customer reviews",
+      value: "+10k",
+    },
+    {
+      id: "stats-3",
+      title: "5 star rating on Play store",
+      value: "+15k",
+    },
+  ];
+
+  export const socialMedia = [
+    {
+      id: "social-media-1",
+      icon: instagram,
+      link: "https://www.instagram.com/",
+    },
+    {
+      id: "social-media-2",
+      icon: facebook,
+      link: "https://www.facebook.com/",
+    },
+    {
+      id: "social-media-3",
+      icon: twitter,
+      link: "https://www.twitter.com/",
+    },
+    {
+      id: "social-media-4",
+      icon: linkedin,
+      link: "https://www.linkedin.com/",
+    },
+  ];
