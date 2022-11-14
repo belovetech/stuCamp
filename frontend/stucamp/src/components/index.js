f4e5b1f9cbf1aa5caeea48798a93cf1525d212b1
Adebayo Samuel <eladebayoor@gmail.com>
2022-11-14 18:56:08 +0100

    [feat]: components created

---
+++ frontend/stucamp/src/components/index.js
@@ -0,0 +1,19 @@
+import Navbar from "./Navbar";
+import Button from "./Button";
+import Hero from "./Hero";
+import Popular from "./Popular";
+import Cities from "./Cities";
+import Offers from "./Offers";
+import Reviews from "./Reviews";
+import Footer from "./Footer";
+
+export {
+    Navbar,
+    Button,
+    Hero,
+    Popular,
+    Cities,
+    Offers,
+    Reviews,
+    Footer
+};
