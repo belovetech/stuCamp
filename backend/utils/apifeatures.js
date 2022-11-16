9d3f3f1582e74dbff82c6991d8ee45b2c1e84df9
belovetech <belovetech@gmail.com>
2022-11-16 10:04:32 +0100

    [Fix]: paginate error

--- backend/utils/apifeatures.js
+++ backend/utils/apifeatures.js
@@ -34,13 +34,11 @@ class APIfeatures {
     return this;
   }
   paginate() {
-    if (this.queryString.page) {
-      const page = this.queryString.page * 1 || 1;
-      const limit = this.queryString.limit * 1 || 100;
-      const skip = (page - 1) * limit;
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
+    const skip = (page - 1) * limit;
 
-      this.query = this.query.skip(skip).limit(limit);
-    }
+    this.query = this.query.skip(skip).limit(limit);
     return this;
   }
 }
