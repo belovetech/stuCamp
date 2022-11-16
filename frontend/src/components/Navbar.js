79df157fdcd49f66002425d0f7eb74b749ca9f6e
Adebayo Samuel <eladebayoor@gmail.com>
2022-11-16 05:14:13 +0100

    [fix]: Navbar MVP completed

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,9 +1,178 @@
-import React from 'react'
+import { Fragment } from 'react'
+import styles from '../style';
+import { logo } from './index.js';
+import { cities } from '../constants'
+import { Popover, Transition } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
 
-const Navbar = () => {
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <Popover className="relative bg-white">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6">
+        <div className={styles.navContainer}>
+          <div className={styles.logoContainer}>
+            <a href="#">
+              <span className="sr-only">stuCamp</span>
+              <img
+                className="h-5 w-auto sm:h-8"
+                src={logo}
+                alt="logo for stucamp"
+              />
+            </a>
+          </div>
+          <div className="-my-2 -mr-2 md:hidden">
+            <Popover.Button className={styles.menu}>
+              <span className="sr-only">Open menu</span>
+              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+            </Popover.Button>
+          </div>
+          <Popover.Group as="nav" className="hidden space-x-10 md:flex">
+
+            <a href="#" className={styles.navLinks}>
+              Home
+            </a>
+            <a href="#" className={styles.navLinks}>
+              About
+            </a>
+            <a href="#" className={styles.navLinks}>
+              Contact Us
+            </a>
+
+            <Popover className="relative">
+              {({ open }) => (
+                <>
+                  <Popover.Button
+                    className={classNames(
+                      open ? 'text-gray-900' : 'text-gray-500',
+                      `${styles.active}`
+                    )}
+                  >
+                    <span>Cities</span>
+                    <ChevronDownIcon
+                      className={classNames(
+                        open ? 'text-gray-600' : 'text-gray-400',
+                        'ml-2 h-5 w-5 group-hover:text-gray-500'
+                      )}
+                      aria-hidden="true"
+                    />
+                  </Popover.Button>
+
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-200"
+                    enterFrom="opacity-0 translate-y-1"
+                    enterTo="opacity-100 translate-y-0"
+                    leave="transition ease-in duration-150"
+                    leaveFrom="opacity-100 translate-y-0"
+                    leaveTo="opacity-0 translate-y-1"
+                  >
+                    <Popover.Panel className="absolute left-1/2 z-10 mt-3 w-screen max-w-lg pl-8 -translate-x-1/2 transform px-2 sm:px-0">
+                      <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                        <div className="relative h-40 pl-3 flex justify-between flex-wrap bg-white px-5 py-6 sm:gap-1 sm:p-4">
+                          {cities.map((item) => (
+                            <a
+                              key={item.name}
+                              href={item.href}
+                              className="rounded-lg w-24 p-1 hover:bg-gray-50"
+                            >
+                              <div className="flex items-center">
+                                <p className="text-base font-medium text-gray-900">{item.name}</p>
+                              </div>
+                            </a>
+                          ))}
+                        </div>
+                      </div>
+                    </Popover.Panel>
+                  </Transition>
+                </>
+              )}
+            </Popover>
+          </Popover.Group>
+          <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
+            <a href="#" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+              Sign in
+            </a>
+            <a
+              href="#"
+              className={styles.button}
+            >
+              Book with confidence
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="duration-200 ease-out"
+        enterFrom="opacity-0 scale-95"
+        enterTo="opacity-100 scale-100"
+        leave="duration-100 ease-in"
+        leaveFrom="opacity-100 scale-100"
+        leaveTo="opacity-0 scale-95"
+      >
+        <Popover.Panel focus className="absolute inset-x-0 top-0 origin-top-right transform p-2 transition md:hidden">
+          <div className="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+            <div className="px-5 pt-5 pb-6">
+              <div className="flex items-center justify-between">
+                <div>
+                  <img
+                    className="h-8 w-auto"
+                    src={logo}
+                    alt="logo for stucamp"
+                  />
+                </div>
+                <div className="-mr-2">
+                  <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                    <span className="sr-only">Close menu</span>
+                    <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                  </Popover.Button>
+                </div>
+              </div>
+            </div>
+            <div className="space-y-6 py-6 px-5">
+              <div className="grid grid-cols-2 gap-y-4 gap-x-8">
+                <a href="#" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                  About
+                </a>
+
+                <a href="#" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                  Contact Us
+                </a>
+                {cities.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="text-base font-medium text-gray-900 hover:text-gray-700"
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+              <div>
+                <a
+                  href="#"
+                  className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+                >
+                  Sign up
+                </a>
+                <p className="mt-6 text-center text-base font-medium text-gray-500">
+                  Existing customer?{' '}
+                  <a href="#" className="text-indigo-600 hover:text-indigo-500">
+                    Sign in
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </Popover.Panel>
+      </Transition>
+    </Popover>
   )
 }
 
-export default Navbar
--- frontend/src/style.js
+++ frontend/src/style.js
@@ -1,4 +1,10 @@
 const styles = {
+    logoContainer: "flex justify-start lg:w-0 lg:flex-1",
+    navContainer: "flex items-center justify-between border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10",
+    menu: "inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500",
+    navLinks: "text-base font-medium text-gray-500 hover:text-gray-900",
+    active: "group inline-flex items-center rounded-md bg-white text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2",
+    button: "ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-3xl border border-transparent bg-primary px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-primary_deep",
     boxWidth: "xl:max-w-[1280px] w-full",
 
     heading2: "font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full",
