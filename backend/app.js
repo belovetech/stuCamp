ecf21e2ab7e6dea084e7a86f851f28a2d1a6f72c
belovetech <belovetech@gmail.com>
2022-11-16 10:05:18 +0100

    [Feat]: create a request time variable

--- backend/app.js
+++ backend/app.js
@@ -13,8 +13,12 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-
 app.use(express.json());
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 app.use('/api/v1/hostels', hostelRouter);
 app.use('/api/v1/users', userRouter);
