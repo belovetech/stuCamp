97c7858fc9b66e1350d10a69a9d3774f6da497ee
belovetech <belovetech@gmail.com>
2022-11-17 01:48:07 +0100

    [Feat]: handle uncaught exception and unhandle rejection error

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,17 @@
+// Handle UNHANDLED REJECTION
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION 🔥 shutting down.....');
+  process.exit(1);
+  // server.close(() => {});
+});
+// Handle UNCAUGHT EXCEPTION
+process.on('uncaughtException', err => {
+  console.log(err.name, err.message);
+  console.log('UNCAUGHT EXCEPTION 🔥 shutting down.....');
+  process.exit(1);
+});
+
 const dotenv = require('dotenv');
 dotenv.config({ path: './.config.env' });
 
