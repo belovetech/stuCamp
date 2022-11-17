bc6b46bc86e793f3d16f532c012f03306a36d972
belovetech <belovetech@gmail.com>
2022-11-17 01:00:29 +0100

    [Feat]: implement document instance methods to check if password has changed after the token was issued

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -6,13 +6,13 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     trim: true,
-    require: [true, 'Please, tell us your name'],
+    required: [true, 'Please, tell us your name'],
   },
   email: {
     type: String,
     unique: true,
     trim: true,
-    require: [true, 'Please, provide youe email'],
+    required: [true, 'Please, provide youe email'],
     lowercase: true,
     validate: [validator.isEmail, 'Please provide a valid email'],
   },
@@ -23,13 +23,13 @@ const userSchema = new mongoose.Schema({
   photo: String,
   password: {
     type: String,
-    require: [true, 'Please provide a password'],
+    required: [true, 'Please provide a password'],
     minLength: 8,
     select: false,
   },
   passwordConfirm: {
     type: String,
-    require: [true, 'Please confirm your password'],
+    required: [true, 'Please confirm your password'],
     validate: {
       validator: function (el) {
         return this.password === el;
@@ -37,6 +37,7 @@ const userSchema = new mongoose.Schema({
       message: 'Passwords are not the same!',
     },
   },
+  passwordChangedAt: Date,
 });
 
 userSchema.pre('save', async function (next) {
@@ -55,6 +56,20 @@ userSchema.methods.correctPassword = async function (
   userPassword
 ) {
   return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    // Convert time to seconds from milliseconds
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+
+    return JWTTimestamp < changedTimestamp;
+  }
+
+  return false;
 };
 
 const User = mongoose.model('User', userSchema);
