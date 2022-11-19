792a156dd6072e8fd7a878f6a3da8bae770d9e81
belovetech <belovetech@gmail.com>
2022-11-19 13:47:02 +0100

    [Feat]: use helmet - security middleware

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -11,20 +12,27 @@ const userRouter = require('./routes/userRouter');
 const app = express();
 
 // GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logger
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit request from the same IP
 const limiter = rateLimit({
   max: 50, // 50 request
   windows: 60 * 60 * 1000, // 1hr
-  message: 'Too many request. Please try again in an hour.',
+  message: 'Too many request from this IP, Please try again in an hour.',
 });
-
 app.use('/api', limiter);
 
+// Body parser reading data from body into req.body
 app.use(express.json());
+// app.use(express.json({ limit: '10kb' }));
 
+// Testing Middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
