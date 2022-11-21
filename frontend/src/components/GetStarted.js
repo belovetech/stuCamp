06df17f43fe132dc2dd1de43ad0b94151fa4625f
Adebayo Samuel <eladebayoor@gmail.com>
2022-11-21 03:24:37 +0100

    [feat]: GetStarted component added

---
+++ frontend/src/components/GetStarted.js
@@ -0,0 +1,9 @@
+import styles from '../style';
+
+const GetStarted = () => (
+    <div className={`mt-8 cursor-pointer ${styles.button_secondary}`}>
+        <p>Get started</p>
+    </div>
+)
+
+export default GetStarted
