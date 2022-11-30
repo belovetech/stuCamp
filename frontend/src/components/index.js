7b02090231b3fa1a9561da2eaa6b8ee956f874cd
Adebayo Samuel <eladebayoor@gmail.com>
2022-11-30 03:44:38 +0100

    [fix]: components update

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -6,6 +6,7 @@ import Search from "./Search";
 import Locations from "./Locations";
 import Offers from "./Offers";
 import Reviews from "./Reviews";
+import Socials from "./Socials";
 import Footer from "./Footer";
 import logo from '../assets/logo.png';
 import discount from '../assets/discount.svg';
@@ -14,11 +15,20 @@ import room from '../assets/room.jpg';
 import abuja from '../assets/abuja.jpg';
 import room1 from '../assets/room1.jpg';
 import badge from '../assets/badge.svg';
+import badge_white from '../assets/badge_white.svg';
 import people from '../assets/people.jpeg';
 import location from '../assets/location.svg';
 import review from '../assets/review.jpg';
 import next from '../assets/next.svg';
 import prev from '../assets/prev.svg';
+import twitter from '../assets/twitter.svg';
+import linkedin from '../assets/linkedin.svg';
+import facebook from '../assets/facebook.svg';
+import instagram from '../assets/instagram.svg';
+import alx from '../assets/alx.png';
+import airbnb from '../assets/airbnb.png';
+import holberton from '../assets/holberton.jpg';
+import mail from '../assets/mail.svg';
 
 export {
     logo,
@@ -26,6 +36,7 @@ export {
     room,
     search,
     badge,
+    badge_white,
     abuja,
     location,
     room1,
@@ -33,11 +44,20 @@ export {
     review,
     next,
     prev,
+    twitter,
+    linkedin,
+    facebook,
+    instagram,
+    alx,
+    airbnb,
+    holberton,
+    mail,
     Navbar,
     Stats,
     GetStarted,
     Hero,
     Search,
+    Socials,
     Locations,
     Offers,
     Reviews,
