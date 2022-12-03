ca0eb28130f81aa55742fc90ea43dc5d30fee347
Adebayo Samuel <eladebayoor@gmail.com>
2022-12-03 02:41:04 +0100

    [style]: made font semibold

--- frontend/src/components/Partners.js
+++ frontend/src/components/Partners.js
@@ -4,7 +4,7 @@ import { alx, airbnb, holberton } from './index';
 const Partners = () => {
   return (
     <div className='my-4'>
-      <h3 className="font-poppins">Our Strategic Partners</h3>
+      <h3 className="font-poppins font-semibold">Our Strategic Partners</h3>
       <ul className="my-4 flex justify-between">
         <li><img className="w-24 h-24" src={alx} alt="alx africa" /></li>
         <li><img className="ml-4 w-24 h-24" src={holberton} alt="holberton" /></li>
