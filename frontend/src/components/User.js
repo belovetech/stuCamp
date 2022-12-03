fdabbc5d2ea9cac07c0ddbafa07006297f994314
Adebayo Samuel <eladebayoor@gmail.com>
2022-12-03 02:45:58 +0100

    [fix]: namefix

--- frontend/src/components/User.js
+++ frontend/src/components/User.js
@@ -1,6 +1,6 @@
 import { PaperClipIcon } from '@heroicons/react/20/solid'
 
-export default function Example() {
+export default function User() {
   return (
     <div className="overflow-hidden bg-white shadow sm:rounded-lg">
       <div className="px-4 py-5 sm:px-6">
