46d9cd88373a681c713d1da79b69675c4ab7b7e3
Adebayo Samuel <eladebayoor@gmail.com>
2022-12-06 07:59:56 +0100

    [fix]: added handleSubmit functionality

--- frontend/src/components/SignIn.js
+++ frontend/src/components/SignIn.js
@@ -3,7 +3,11 @@ import { signin } from './index.js'
 import { LockClosedIcon } from '@heroicons/react/20/solid'
 import { Link } from 'react-router-dom'
 
-const SignIn = () => (
+const SignIn = () => {
+  const handleSubmit = () => {
+
+  }
+  return (
   <section id="home" className={`bg-secondary ${styles.flexStart}`}>
     <div
       className={`${styles.boxWidth} sm:pb-16 pb-6 my-16 flex justify-center content-between flex-row bg-secondary ${styles.paddingY}`}
@@ -45,7 +49,7 @@ const SignIn = () => (
               </Link>
             </p>
           </div>
-          <form className="mt-8 space-y-6" action="#" method="POST">
+          <form className="mt-8 space-y-6" action="#" method="POST" onSubmit={handleSubmit}>
             <input type="hidden" name="remember" defaultValue="true" />
             <div className="-space-y-px rounded-md shadow-sm">
               <div>
@@ -60,6 +64,7 @@ const SignIn = () => (
                   required
                   className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                   placeholder="Email address"
+                  // handleChange={handleChange}
                 />
               </div>
               <div>
@@ -124,5 +129,6 @@ const SignIn = () => (
     </div>
   </section>
 )
+}
 
 export default SignIn
