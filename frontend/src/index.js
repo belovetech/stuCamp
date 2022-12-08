e2ddb0c33e5d342c55316c01f92cc2b245b8a935
Adebayo Samuel <eladebayoor@gmail.com>
2022-12-08 12:24:31 +0100

    [fix]: redux capabilities added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { reducers } from './reducers';
 import { BrowserRouter } from "react-router-dom"
+
+const store = createStore(
+  reducers,
+  compose(applyMiddleware(thunk))
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+  </Provider>
 );
