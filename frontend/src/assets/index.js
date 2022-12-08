c8857467fcda777f5a6fe0993a39b44aeea9852f
Adebayo Samuel <eladebayoor@gmail.com>
2022-12-08 12:14:13 +0100

    [feat]: new assets added with index file for indexing assets

---
+++ frontend/src/assets/index.js
@@ -0,0 +1,105 @@
+import logo from './logo.png';
+import discount from './discount.svg';
+import search from './search.svg';
+import room from './room.jpg';
+import abuja from './abuja.jpg';
+import anambra from './anambra.jpg';
+import bayelsa from './bayelsa.jpg';
+import borno from './borno.jpg';
+import delta from './delta.jpg';
+import edo from './edo.jpg';
+import enugu from './enugu.jpg';
+import ibadan from './ibadan.jpg';
+import imo from './imo.jpg';
+import kano from './kano.jpg';
+import lagos from './lagos.jpg';
+import ondo from './ondo.jpg';
+import ogun from './ogun.jpg';
+import osun from './osun.jpg';
+import ph from './ph.jpg';
+import sokoto from './sokoto.jpg';
+import room1 from './room1.jpg';
+import room2 from './room2.jpg';
+import room3 from './room3.jpg';
+import room4 from './room4.jpg';
+import badge from './badge.svg';
+import badge_white from './badge_white.svg';
+import people1 from './people1.jpg';
+import people2 from './people2.jpg';
+import people3 from './people3.jpeg';
+import location from './location.svg';
+import review1 from './review1.jpg';
+import review2 from './review2.jpg';
+import review3 from './review3.jpg';
+import contact from './contact.jpg';
+import about from './about.jpg';
+import avatar from './avatar.svg';
+import next from './next.svg';
+import prev from './prev.svg';
+import twitter from './twitter.svg';
+import linkedin from './linkedin.svg';
+import linkedin2 from './linkedin2.svg';
+import github from './github.svg';
+import facebook from './facebook.svg';
+import instagram from './instagram.svg';
+import alx from './alx.png';
+import airbnb from './airbnb.png';
+import holberton from './holberton.jpg';
+import mail from './mail.svg';
+import signin from './signin.svg';
+
+// // path.join(__dirname, 'assets')
+// const path = require('path');
+// console.log(path.join(__dirname, 'assets'));
+
+export {
+    logo,
+    discount,
+    room,
+    search,
+    badge,
+    badge_white,
+    abuja,
+    anambra,
+    bayelsa,
+    borno,
+    delta,
+    edo,
+    enugu,
+    ibadan,
+    imo,
+    kano,
+    lagos,
+    ondo,
+    ogun,
+    osun,
+    ph,
+    sokoto,
+    location,
+    room1,
+    room2,
+    room3,
+    room4,
+    people1,
+    people2,
+    people3,
+    review1,
+    review2,
+    review3,
+    contact,
+    about,
+    next,
+    prev,
+    avatar,
+    twitter,
+    linkedin,
+    linkedin2,
+    github,
+    facebook,
+    instagram,
+    alx,
+    airbnb,
+    holberton,
+    mail,
+    signin,
+};
